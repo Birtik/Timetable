b2478d3ce719a23051dd74fa4a6c8cd6c905c1c8
Bartosz Widera <wb39480@zut.edu.pl>
2021-02-16 19:05:25 +0100

    fix

--- conf.php
+++ conf.php
@@ -7,6 +7,7 @@ const DB_NAME = "ex0";
 const DB_USER = "root";
 const DB_PASS = "";
 
+
 try{
     return new PDO(Connect::getDSN(DB_NAME,DB_HOST),DB_USER,DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"));
 } 
